af501b0871ab471d0679b9f9f4e26c220a20de5b
hager <hager.mohamed20002@gmail.com>
2025-09-05 22:35:59 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Generative AI project",
+    version= "0.0.0",
+    author= "Hager Mohamed",
+    author_email= "hager.mohamed20002@gmail.com",
+    packages= find_packages(),
+    install_requires= []
+)
